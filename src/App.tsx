f509a962a60a2203f5bd2f78b02152b85dc239a4
Mark-Zinger <lllessive@gmail.com>
2022-03-11 11:56:52 +0700

    add terain

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 
+
+
+import * as THREE from 'three'
+import React, { useRef, useState, Suspense } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls, Plane } from '@react-three/drei';
+import Terrain from "./Terrain";
+import { useControls } from 'leva';
+
+function Box(props: JSX.IntrinsicElements['mesh']) {
+  const ref = useRef<THREE.Mesh>(null!)
+  const [hovered, hover] = useState(false)
+  const [clicked, click] = useState(false)
+  useFrame((state, delta) => (ref.current.rotation.x += 0.01))
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
+
+
+
+
+
+
+
+
 function App() {
+  
+  const { grid } = useControls({ grid: true })
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <Canvas>
+        <ambientLight intensity={0.3} />
+        <pointLight position={[0, 100, 0]} intensity={0.5} />
+        {/*<Box position={[-1.2, 0, 0]} />*/}
+        {/*<Box position={[1.2, 0, 0]} />*/}
+        { grid &&<gridHelper args={[40,40, 0x000, 0x333333]}/> }
+        <OrbitControls/>
+        <Plane args={[1,1]}/>
+        <Suspense fallback={null}>
+          <Terrain/>
+        </Suspense>
+      </Canvas>
     </div>
   );
 }
